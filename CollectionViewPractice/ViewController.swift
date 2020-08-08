d53fcdd6baf8a33d2d55156e3557c16a524077ab
Sagar Ali <sagar18.iosdeveloper@gmail.com>
2020-08-08 19:25:22 +0500

    Initial Commit

---
+++ CollectionViewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewPractice
+//
+//  Created by Sagar Baloch on 08/08/2020.
+//  Copyright © 2020 Sagar Baloch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
